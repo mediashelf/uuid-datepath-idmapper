22f8450463e5e9c4835dc1aa1882be1e03015d49
Edwin Shin <edwin.shin@yourmediashelf.com>
2012-01-13 00:53:36 +0800

    misc site cleanup

--- src/main/java/com/yourmediashelf/fedora/akubra/UUIDDatePathIdMapper.java
+++ src/main/java/com/yourmediashelf/fedora/akubra/UUIDDatePathIdMapper.java
@@ -16,9 +16,6 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with uuid-datepath-idmapper.  If not, see <http://www.gnu.org/licenses/>.
  */
-/**
- * 
- */
 package com.yourmediashelf.fedora.akubra;
 
 import java.net.URI;
